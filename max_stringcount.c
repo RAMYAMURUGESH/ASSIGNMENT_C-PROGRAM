7f3f021c1bbb22e8c9b5b1c869b8080a6bb0ded1
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2017-12-25 23:39:20 +0530

    Create max_stringcount.c

---
+++ max_stringcount.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	int i,count[50],n,j,max=0;
+	char ch[50];
+	printf("ENTER THE STRING:\n");
+	scanf("%s",ch);
+	n=strlen(ch);
+	for(i=0;i<n;i++)
+	{
+		count[i]=1;
+	}
+	for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(ch[i]==ch[j])
+			{
+				count[i]++;
+				ch[j]='\0';
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		if(ch[i]!='\0')
+		{
+			printf("\nTHE COUNT OF CHARACTER %c is %d",ch[i],count[i]);
+			if(count[i]>max)
+			{
+				max=count[i];
+			}
+		}
+	}
+	printf("\nTHE MAXIMUM COUNT IS %d",max);
+	for(i=0;i<n;i++)
+	{
+		if(count[i]==max)
+		{
+			printf("\nTHE MAXIMUM COUNT  POSSESED CHARACTER IS %c",ch[i]);	
+		}
+	}
+	
+}
