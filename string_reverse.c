49787b868706a6a8351d1f5682bdfc922dde4aef
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-01-22 19:52:18 +0530

    Create string_reverse.c

---
+++ string_reverse.c
@@ -0,0 +1,10 @@
+//c program to reverse the string
+
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char a[50]="Hello World";
+	printf("THE STRING BEFORE REVERSING:\n%s",a);
+	printf("\nTHE STRING AFTER REVERSING: \n%s",strrev(a));
+}
